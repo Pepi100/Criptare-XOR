cc872b0d84031675bb0d48929c0e8273eec42df9
Pepi100 <radunedelcu02@gmail.com>
2021-12-02 13:48:18 +0200

    Citire argumente

---
+++ encrypt.py
@@ -0,0 +1,21 @@
+import sys
+
+# python encrypt parolamea2021 input.txt output
+
+
+
+operatie = sys.argv[0]
+cheie = sys.argv[1]
+input_file = sys.argv[2]
+output_file = sys.argv[3]
+
++
+
+
+
+#text = list(open("input.txt").read())
+#iesire = open("output", 'w')
+#parola = input()
+#for x in range(len(text)):
+#    text[x] = chr(ord(text[x]) ^ ord(parola[x % len(parola)]))
+#print("".join(text), file=iesire)
--- main.py
+++
@@ -1,6 +0,0 @@
-text = list(open("input.txt").read())
-iesire = open("output", 'w')
-parola = input()
-for x in range(len(text)):
-    text[x] = chr(ord(text[x]) ^ ord(parola[x % len(parola)]))
-print("".join(text), file=iesire)
