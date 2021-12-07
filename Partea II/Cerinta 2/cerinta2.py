31459b1bf3f82b08173afe48523cc794e2eb9c62
993m <cioclov_maria@yahoo.com>
2021-12-07 03:02:58 +0200

    final cerinta 2

--- Partea II/Cerinta 2/cerinta2.py
+++ Partea II/Cerinta 2/cerinta2.py
@@ -3,25 +3,24 @@ text_criptat = open("output_baieti.txt", 'r').read()
 lista_bytes = [text_criptat[8*i:8*i+8] for i in range(len(text_criptat)//8)]
 
 #############################################lungimea parolei:
-min_hamming = 1000
-for posibila_lungime in range(10,16):
-    suma_hamming = 0
-    n = len(lista_bytes) // posibila_lungime
-    n //= 2
-    for x in range(n - 2):
-        sir1 = "".join(lista_bytes[x * posibila_lungime: x * posibila_lungime + posibila_lungime])
-        for y in range(x + 1, n - 1):
-            sir2 = "".join(lista_bytes[y * posibila_lungime: y * posibila_lungime + posibila_lungime])
-            distanta_hamming = 0
-            for i in range(posibila_lungime):
-                if sir1[i] != sir2[i]:
-                    distanta_hamming += 1
-            suma_hamming += distanta_hamming
-    suma_hamming = suma_hamming / (n * (n - 1) // 2)
-    if suma_hamming < min_hamming:
-        min_hamming = suma_hamming
-        lungime_cheie = posibila_lungime
-    print(lungime_cheie)
+# min_hamming = 1000
+# for posibila_lungime in range(10,16):
+#     suma_hamming = 0
+#     n = len(lista_bytes) // posibila_lungime
+#     for x in range(n - 2):
+#         sir1 = "".join(lista_bytes[x * posibila_lungime: x * posibila_lungime + posibila_lungime])
+#         for y in range(x + 1, n - 1):
+#             sir2 = "".join(lista_bytes[y * posibila_lungime: y * posibila_lungime + posibila_lungime])
+#             distanta_hamming = 0
+#             for i in range(posibila_lungime):
+#                 if sir1[i] != sir2[i]:
+#                     distanta_hamming += 1
+#             suma_hamming += distanta_hamming
+#     suma_hamming = suma_hamming / (n * (n - 1) // 2)
+#     if suma_hamming < min_hamming:
+#         min_hamming = suma_hamming
+#         lungime_cheie = posibila_lungime
+#     print(lungime_cheie)
 #############################################
 
 
@@ -29,34 +28,34 @@ lungime_cheie = 12
 
 
 
-# dictionar[x] contine toate caracterele care sunt xorate cu elementul de pe pozitia x din parola
+# valori_pozitie contine toate caracterele care sunt xorate cu elementul de pe pozitia x din parola
 
-dict = {}
+valori_pozitie = []
 for x in range(lungime_cheie):
-    dict[x] = set()
+    valori_pozitie += [set()]
     for y in range(x,len(lista_bytes),lungime_cheie):
-        dict[x].add(chr(int(lista_bytes[y],2)))
+        valori_pozitie[-1].add(chr(int(lista_bytes[y],2)))
 
 
-# l contine toate caracterele care s-ar putea gasi in parola
+# caractere_parola contine toate caracterele care s-ar putea gasi in parola
 
-l = [chr(x) for x in range(ord('a'),ord('z') + 1)]
-l += [chr(x) for x in range(ord('A'),ord('Z') + 1)]
-l += [chr(x) for x in range(ord('0'), ord('0') + 10)]
+caractere_parola = [chr(x) for x in range(ord('a'),ord('z') + 1)]
+caractere_parola += [chr(x) for x in range(ord('A'),ord('Z') + 1)]
+caractere_parola += [chr(x) for x in range(ord('0'), ord('0') + 10)]
 
 
 # rezultat contine toate caracterele care se pot regasi in fisierul input.txt
 
-rezultat = l + [',', '"', ':', '.', '!', ';', ' ', "'", '\n', '-', '?']
+rezultat = caractere_parola + [',', '"', ':', '.', '!', ';', ' ', "'", '\n', '-', '?', ')', '(', chr(9)]
 
 
 
 dict_posibile_caractere = {}
 for x in range(lungime_cheie):
     dict_posibile_caractere[x] = []
-    for posibil_caracter in l:
+    for posibil_caracter in caractere_parola:
         ok = 1
-        for caracter in dict[x]:
+        for caracter in valori_pozitie[x]:
             rez = ord(caracter) ^ ord(posibil_caracter)
             if chr(rez) not in rezultat:
                 ok = 0
